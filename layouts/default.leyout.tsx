13cf03116e066d9d3f42c45dc0e739ece413031b
aliakbar8099 <aliakbar.n8099@gmail.com>
2023-03-29 05:06:18 +1400

    fixed Type error: 'open' is declared but its value is never read.

--- layouts/default.leyout.tsx
+++ layouts/default.leyout.tsx
@@ -5,8 +5,6 @@ type DefaultLayoutProps = {
 };
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
-    const [open, setOpen] = useState<Boolean>(false);
-
     return (
         <>
             <div className="w-full overflow-auto mt-[5rem] -mb-10">
